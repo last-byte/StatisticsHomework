d3588b79f7b29190ac255146ff727ab9f595c1c0
last-byte <fed.lag@protonmail.ch>
2022-11-30 17:45:16 +0100

    edited interface

--- HW5.1_CS/Histogram/Histogram/Form1.Designer.cs
+++ HW5.1_CS/Histogram/Histogram/Form1.Designer.cs
@@ -33,22 +33,26 @@
             // 
             // drawButton
             // 
-            this.drawButton.Location = new System.Drawing.Point(361, 374);
+            this.drawButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.drawButton.ForeColor = System.Drawing.Color.Snow;
+            this.drawButton.Location = new System.Drawing.Point(413, 499);
+            this.drawButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.drawButton.Name = "drawButton";
-            this.drawButton.Size = new System.Drawing.Size(114, 27);
+            this.drawButton.Size = new System.Drawing.Size(130, 36);
             this.drawButton.TabIndex = 0;
             this.drawButton.Text = "Draw";
-            this.drawButton.UseVisualStyleBackColor = true;
+            this.drawButton.UseVisualStyleBackColor = false;
             this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.drawButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Histogram Drawer";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
