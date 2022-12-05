801282da24c6f9f64843020225e0d6ca8b6b7453
Federico Lagrasta <fed.lag@protonmail.ch>
2022-12-05 09:45:44 +0100

    fixed structure

--- HW2.3_VB/Form1.vb
+++ HW2.3_VB/Form1.vb
@@ -12,7 +12,7 @@ Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim Path As String = "C:\StudentClass.csv"
+        Dim Path As String = "..\..\..\StudentClass.csv"
         Using R As New StreamReader(Path)
             Do While Not R.EndOfStream
 
